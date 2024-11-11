094f4e4725f49d86959209e5e539ae7d395b62d9
fernandatc17 <abigailticlavilca46@gmail.com>
2024-11-11 16:47:46 -0500

    first commit

---
+++ app/src/main/java/com/ticlavilca/abigail/poketinder/PokemonApi.kt
@@ -0,0 +1,10 @@
+package com.ticlavilca.abigail.poketinder
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface PokemonApi {
+    @GET ("api/v2/pokemon")
+    suspend fun getPokemons(): Response<PokemonListResponse>
+
+}
